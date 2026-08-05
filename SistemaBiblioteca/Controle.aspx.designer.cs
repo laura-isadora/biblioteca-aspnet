668f7e498442a9ad3e4bf4bf1825c26289f46ac5
Laura Isadora <laura.moreira17@hotmail.com>
2026-08-05 15:08:02 -0300

    atualização da lógica p/ função Visible de gvUsuarios/gvLivros

--- SistemaBiblioteca/Controle.aspx.designer.cs
+++ SistemaBiblioteca/Controle.aspx.designer.cs
@@ -78,6 +78,15 @@ namespace SistemaBiblioteca
         protected global::System.Web.UI.WebControls.GridView gvUsuarios;
 
         /// <summary>
+        /// gvLivros control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvLivros;
+
+        /// <summary>
         /// Cadastros control.
         /// </summary>
         /// <remarks>
